a913d0383d761115c8c882e89d631ce29f6e71d0
michaelGuo1204 <2236536804@qq.com>
2022-03-28 19:42:16 +0800

    Explainer improved, got two layout to analysis

--- GNN/src/Explain.py
+++ GNN/src/Explain.py
@@ -1,47 +1,83 @@
 from spektral.data import DisjointLoader
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+import pandas
+from spektral.data import DisjointLoader
+from spektral.models import GeneralGNN, GNNExplainer
 from spektral.transforms.normalize_adj import NormalizeAdj
 
 from Net import *
 from Utili import *
 
 ################################################################################
+# Hyper Params
+################################################################################
+a_threshold_list = [0.35, 0.345, 0.34, 0.335, 0.33, 0.325, 0.32, 0.31]  # HIGHEST FIRST!!!
+layouts = [nx.layout.spring_layout, nx.layout.circular_layout]
+batch_size = 1
+epochs = 300
+################################################################################
 # Load data
 ################################################################################
-batch_size = 32
-epochs = 300
-data = WDataset(load=True, n_traits=100, transforms=NormalizeAdj())
-
-# Train/valid/test split
-idxs = np.random.permutation(len(data))
-split_va, split_te = int(0.8 * len(data)), int(0.9 * len(data))
-idx_tr, idx_va, idx_te = np.split(idxs, [split_va, split_te])
-data_tr = data[idx_tr]
-data_va = data[idx_va]
-data_te = data[idx_te]
-
+file = pandas.read_csv('../Data/fs.csv')
+header = file.columns[1:-1]
+data = WDataset(load=True, n_traits=100, transforms=NormalizeAdj())[10000, 10001]
 # Data loaders
-loader_tr = DisjointLoader(data_tr, batch_size=batch_size, epochs=epochs)
-loader_va = DisjointLoader(data_va, batch_size=batch_size)
-loader_te = DisjointLoader(data_te, batch_size=batch_size)
+loader = DisjointLoader(data, batch_size=batch_size, epochs=epochs)
 
 ################################################################################
-# Explain model
+# Load Model
 ################################################################################
 
 model = GeneralGNN(data.n_labels, activation='softmax')
 model.built = True
 model.load_weights('./logs/1647925223.765264/variables/variables')
-(x, a, i), y = next(loader_te)
-
+while True:
+    (x, a, i), y = next(loader)
+    if (y[0, :] == [0, 1]).all(): break
 cut_idx = (i == 0).sum()
 x_exp = x[:cut_idx]
 a_exp = tf.sparse.slice(a, start=[0, 0], size=[cut_idx, cut_idx])
-explainer = GNNExplainer(model, graph_level=True, x_size_coef=0.001)
+explainer = GNNExplainer(model, graph_level=True)
 
 # Explain prediction for one graph
 adj_mask, feat_mask = explainer.explain_node(x=x_exp, a=a_exp)
 
-# Plot the result
-G = explainer.plot_subgraph(adj_mask, feat_mask)
-plt.savefig('Sub.png')
-plt.show()
+#####################################################################
+# Visualized the data
+###################################################################
+for layout in layouts:
+    highest = True
+    group = {}
+    for a_threshold in a_threshold_list:
+        # Get the sub graph thresholded by a
+        G_r = explainer.plot_subgraph(adj_mask, feat_mask, a_thresh=a_threshold)
+        # TODO: Integrated the code into spektral
+        plt.close()  # Close first cuz the plot_subgraph will draw on plt
+        plt.figure(dpi=1200)
+        giant = max(nx.algorithms.components.connected_components(G_r), key=len)  # Select biggest connected subgraph
+        G = G_r.subgraph(giant)
+        pos = layout(G)  # Choose the circular form
+        G_unf = nx.Graph(G)  # Unfrozen the graph to remove the self looped
+        G_unf.remove_edges_from(nx.selfloop_edges(G_unf))
+        G = G_unf
+
+        # Find the biggest smallest 0.9 biggest degrees
+        d = dict(G.degree)
+        low, *_, high = sorted(d.values())
+        norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
+        mapper = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.coolwarm)  # Create color map
+        if highest:
+            group = dict(G.degree)
+            highest = False
+        qua = np.quantile(list(d.values()), 0.75)
+        # Draw!
+        labels = {node: header[node] for (node, val) in d.items() if node in group or val > qua}
+        nx.draw_networkx_nodes(G, pos=pos, node_size=30, node_color=[mapper.to_rgba(i) for i in d.values()])
+        nx.draw_networkx_edges(G, pos=pos, edge_color="grey", alpha=0.8)
+        nx.draw_networkx_labels(
+            G, pos=pos, labels=labels, font_color="black", font_size=10, verticalalignment="bottom"
+        )
+        name = str(layout).split(' ')[1].split('_')[0]
+        plt.savefig('../result/10k GNN explainer/{}Sub{}.png'.format(name, a_threshold))
